efc804719d01bec7c7d9a4f1506b84865ab136de
depgeinfdeveloper <depgeinf.developer@gmail.com>
2022-05-26 20:24:52 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainHeaderComponent } from './common/main-header/main-header.component';
+import { MainNavComponent } from './common/main-nav/main-nav.component';
+import { MainFooterComponent } from './common/main-footer/main-footer.component';
+import { InicioComponent } from './home/inicio/inicio.component';
+import { MainBannerComponent } from './home/main-banner/main-banner.component';
+import { LastNewsComponent } from './home/last-news/last-news.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainNavComponent,
+    MainFooterComponent,
+    InicioComponent,
+    MainBannerComponent,
+    LastNewsComponent,
   ],
   imports: [
     BrowserModule
